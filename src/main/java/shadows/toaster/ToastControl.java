aa223b77709420c22f779fd09eb0553fb4d45070
Shadows_of_Fire <Bward7864@gmail.com>
2018-03-24 16:58:53 -0400

    Closes #3
    I completely forgot about this

--- src/main/java/shadows/toaster/ToastControl.java
+++ src/main/java/shadows/toaster/ToastControl.java
@@ -28,7 +28,7 @@ public class ToastControl {
 
 	public static final String MODID = "toastcontrol";
 	public static final String MODNAME = "Toast Control";
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.4.1";
 	public static final String VERS = "[1.12, 1.13)";
 	public static final String DEPS = "required-after:placebo@[1.2.0,)";
 	
