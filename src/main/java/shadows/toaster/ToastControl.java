66b7ab0e2ae26ae195282867695ecff1eb665707
Shadows_of_Fire <Bward7864@gmail.com>
2018-09-06 02:49:27 -0400

    1.7.1

--- src/main/java/shadows/toaster/ToastControl.java
+++ src/main/java/shadows/toaster/ToastControl.java
@@ -31,9 +31,9 @@ public class ToastControl {
 
 	public static final String MODID = "toastcontrol";
 	public static final String MODNAME = "Toast Control";
-	public static final String VERSION = "1.7.0";
+	public static final String VERSION = "1.7.1";
 	public static final String VERS = "[1.12, 1.13)";
-	public static final String DEPS = "required-after:placebo@[1.2.0,)";
+	public static final String DEPS = "required-after:placebo@[1.4.1,)";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
 	public static final KeyBinding CLEAR = new KeyBinding("key.toastcontrol.clear", Keyboard.KEY_J, "key.toastcontrol.category");
