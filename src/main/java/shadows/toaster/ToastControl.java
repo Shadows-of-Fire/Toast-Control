154d908f125743dd20662dfab6248290f27bb7d6
Shadows_of_Fire <Bward7864@gmail.com>
2019-05-04 22:32:48 -0400

    Closes #6

--- src/main/java/shadows/toaster/ToastControl.java
+++ src/main/java/shadows/toaster/ToastControl.java
@@ -26,14 +26,14 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import shadows.toaster.BetterGuiToast.BetterToastInstance;
 
-@Mod(modid = ToastControl.MODID, version = ToastControl.VERSION, name = ToastControl.MODNAME, acceptedMinecraftVersions = ToastControl.VERS, clientSideOnly = true, dependencies = ToastControl.DEPS)
+@SuppressWarnings("deprecation")
+@Mod(modid = ToastControl.MODID, version = ToastControl.VERSION, name = ToastControl.MODNAME, clientSideOnly = true, dependencies = ToastControl.DEPS)
 public class ToastControl {
 
 	public static final String MODID = "toastcontrol";
 	public static final String MODNAME = "Toast Control";
-	public static final String VERSION = "2.0.0";
-	public static final String VERS = "[1.12, 1.13)";
-	public static final String DEPS = "required-after:placebo@[2.0.0,)";
+	public static final String VERSION = "1.8.1";
+	public static final String DEPS = "required-after:placebo@[1.6,)";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
 	public static final KeyBinding CLEAR = new KeyBinding("key.toastcontrol.clear", Keyboard.KEY_J, "key.toastcontrol.category");
@@ -43,7 +43,7 @@ public class ToastControl {
 
 	@SubscribeEvent
 	public void keys(KeyInputEvent e) {
-		if (Keyboard.isKeyDown(CLEAR.getKeyCode()) && CLEAR.isPressed()) Minecraft.getMinecraft().getToastGui().clear();
+		if (CLEAR.isKeyDown() && CLEAR.isPressed()) Minecraft.getMinecraft().getToastGui().clear();
 	}
 
 	@EventHandler
