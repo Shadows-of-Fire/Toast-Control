e98d3c0ea4c4bcc8b09d51ed5848363aef4afa5d
Shadows_of_Fire <Bward7864@gmail.com>
2020-07-08 13:59:00 -0400

    Port to 1.16

--- src/main/java/shadows/toaster/BetterGuiToast.java
+++ src/main/java/shadows/toaster/BetterGuiToast.java
@@ -2,6 +2,7 @@ package shadows.toaster;
 
 import java.util.Arrays;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.Minecraft;
@@ -20,14 +21,14 @@ public class BetterGuiToast extends ToastGui {
 	}
 
 	@Override
-	public void render() {
+	public void func_238541_a_(MatrixStack stack) {
 		if (!this.mc.gameSettings.hideGUI) {
 			RenderHelper.disableStandardItemLighting();
 
 			for (int i = 0; i < this.visible.length; ++i) {
 				ToastInstance<?> toastinstance = this.visible[i];
 
-				if (toastinstance != null && toastinstance.render(this.mc.getWindow().getScaledWidth(), i)) {
+				if (toastinstance != null && toastinstance.render(this.mc.getMainWindow().getScaledWidth(), i, stack)) {
 					this.visible[i] = null;
 				}
 
@@ -65,7 +66,7 @@ public class BetterGuiToast extends ToastGui {
 		}
 
 		@Override
-		public boolean render(int scaledWidth, int arrayPos) {
+		public boolean render(int scaledWidth, int arrayPos, MatrixStack stack) {
 			long i = Util.milliTime();
 
 			if (this.animationTime == -1L) {
@@ -77,14 +78,14 @@ public class BetterGuiToast extends ToastGui {
 				this.visibleTime = i;
 			}
 
-			RenderSystem.pushMatrix();
-			if (ToastConfig.INSTANCE.startLeft.get()) RenderSystem.translatef(-160 + 160 * this.getVisibility(i), arrayPos * 32, 500 + arrayPos);
-			else RenderSystem.translatef(scaledWidth - 160F * this.getVisibility(i), arrayPos * 32, 500 + arrayPos);
+			stack.push();
+			if (ToastConfig.INSTANCE.startLeft.get()) stack.translate(-160 + 160 * this.getVisibility(i), arrayPos * 32, 500 + arrayPos);
+			else stack.translate(scaledWidth - 160F * this.getVisibility(i), arrayPos * 32, 500 + arrayPos);
 			RenderSystem.enableBlend();
-			RenderSystem.translatef(ToastConfig.INSTANCE.offsetX.get(), ToastConfig.INSTANCE.offsetY.get(), 0);
-			IToast.Visibility itoast$visibility = toast.draw(BetterGuiToast.this, i - this.visibleTime);
+			stack.translate(ToastConfig.INSTANCE.offsetX.get(), ToastConfig.INSTANCE.offsetY.get(), 0);
+			IToast.Visibility itoast$visibility = toast.func_230444_a_(stack, BetterGuiToast.this, i - this.visibleTime);
 			RenderSystem.disableBlend();
-			RenderSystem.popMatrix();
+			stack.pop();
 
 			if (this.forcedShowTime > ToastConfig.INSTANCE.forceTime.get() && itoast$visibility != this.visibility) {
 				this.animationTime = i - ((long) ((1 - this.getVisibility(i)) * 600));
