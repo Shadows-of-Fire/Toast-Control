7930486b6ef69ecd01606507676fa80771f5eba4
Shadows_of_Fire <Bward7864@gmail.com>
2020-08-06 22:25:55 -0400

    register displaytest extension

--- src/main/java/shadows/toaster/ToastLoader.java
+++ src/main/java/shadows/toaster/ToastLoader.java
@@ -1,15 +1,18 @@
 package shadows.toaster;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(ToastLoader.MODID)
 public class ToastLoader {
@@ -24,6 +27,7 @@ public class ToastLoader {
 			MinecraftForge.EVENT_BUS.register(ToastConfig.class);
 			ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ToastConfig.SPEC);
 		} else LOGGER.error("Running on a dedicated server, disabling mod.");
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 
 }
